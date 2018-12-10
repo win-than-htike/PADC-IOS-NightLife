92c4dbc1313d901f6a605b3d7a5753287bd7f70f
phyothinzaraung <phyothinzar.ag92@gmail.com>
2018-12-10 15:01:23 +0630

    list and detail screen commit

--- PADC-IOS-NightLife/Cell/RestaurantCollectionViewCell.swift
+++ PADC-IOS-NightLife/Cell/RestaurantCollectionViewCell.swift
@@ -2,17 +2,12 @@
 //  RestaurantCollectionViewCell.swift
 //  PADC-IOS-NightLife
 //
-//  Created by Phyo Thinzar Aung on 12/3/18.
+//  Created by Phyo Thinzar Aung on 12/6/18.
 //  Copyright © 2018 padcmyanmar. All rights reserved.
 //
 
 import UIKit
 
 class RestaurantCollectionViewCell: UICollectionViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
+    
 }
--- PADC-IOS-NightLife/Controller/RestaurantsViewController.swift
+++ PADC-IOS-NightLife/Controller/RestaurantsViewController.swift
@@ -10,24 +10,39 @@ import UIKit
 
 class RestaurantsViewController: UIViewController {
 
+    @IBOutlet weak var collectionViewRestaurants: UICollectionView!
+    
+    
     @IBAction func back(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        collectionViewRestaurants.delegate = self
+        collectionViewRestaurants.dataSource = self
+    }
+
+}
+
+extension RestaurantsViewController : UICollectionViewDelegate{
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let navigationController = UIStoryboard(name: "Restaurant", bundle: nil).instantiateViewController(withIdentifier: "RestaurantDetailViewController") as! UINavigationController
+        self.present(navigationController, animated: true, completion: nil)
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension RestaurantsViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RestaurantCollectionViewCell", for: indexPath) as! RestaurantCollectionViewCell
+        return cell
+    }
+    
+    
 }
