a614e4c57e6866e7fbb5b533c6cc542c032a6dae
phyothinzaraung <phyothinzar.ag92@gmail.com>
2018-12-13 17:22:56 +0630

    Screen Design for Bar

--- PADC-IOS-NightLife/Controller/BarsViewController.swift
+++ PADC-IOS-NightLife/Controller/BarsViewController.swift
@@ -10,24 +10,57 @@ import UIKit
 
 class BarsViewController: UIViewController {
 
+    @IBOutlet weak var collectionViewBars: UICollectionView!
     @IBAction func back(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionViewBars.delegate = self
+        collectionViewBars.dataSource = self
+        
+        registerCell()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func registerCell()  {
+        CellRegisterUtils.cellRegister(nibName: "BarPromotionCollectionViewCell", collectionView: collectionViewBars)
+        CellRegisterUtils.cellRegister(nibName: "BarCollectionViewCell", collectionView: collectionViewBars)
     }
-    */
 
+}
+
+extension BarsViewController : UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        }else {
+            return 10
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.section == 0 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BarPromotionCollectionViewCell", for: indexPath) as! BarPromotionCollectionViewCell
+            return cell
+        }else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BarCollectionViewCell", for: indexPath) as! BarCollectionViewCell
+            return cell
+        }
+    }
+}
+
+extension BarsViewController : UICollectionViewDelegate {
+    
+}
+
+extension BarsViewController : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.frame.width, height: 250)
+    }
 }
